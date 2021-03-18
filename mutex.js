e42110fca9aae076093fe668115c70f53ae8f763
Gary Huong <garyhuong@gmail.com>
2021-03-18 17:54:11 -0400

    Revert "Refactor to track the waiting list for the mutex"
    This reverts commit 4ea4b4256beedfe6d23228f0f153077d0ade4db8.

--- mutex.js
+++ mutex.js
@@ -6,25 +6,8 @@
  * For more, see: https://www.nodejsdesignpatterns.com/blog/node-js-race-conditions/
  */
 function Mutex() {
-  // The exclusive lock that only one (concurrent) task may hold:
-  let _mutex = {
-    lock: Promise.resolve(), // "resolved" promise represents "unlocked"
-    key: null, // doesn't need a key
-  };
-
-  // a queue of requests lining up for the mutex
-  // holds objects: { lock, key }, where:
-  // - lock: is a promise that gives away its resolve function.
-  // - key: is a promise of said resolve function. The caller is meant to call it when they're done with the lock.
-  //        The key-promise is then'd onto the previous lock (so only once the previous lock resolves, THEN the key does)
-  let _lineUp = [];
-
-  const _noLineUp = () => _lineUp.length === 0; // whether there's a line up
-  const _lastInLine = () => _lineUp[_lineUp.length - 1]; // who to wait after
-  const _nextInLine = () => _lineUp[0]; // who's next in line to be admitted
-  const _waitInLine = (lock, key) => _lineUp.push({ lock, key }); // line up at the end
-  const _cutInLine = (lock, key) => _lineUp.unshift({ lock, key }); // cut to the front
-  const _admitNext = () => _mutex = _lineUp.shift(); // admit next in line
+  //* The mutually exclusive lock that only one (concurrent) task at a time may hold:
+  let _currentLock = Promise.resolve(); // "resolved" represents "unlocked"
 
   /**
    * * Attempt to acquire the currentLock
@@ -38,23 +21,19 @@ function Mutex() {
   this.lock = () => {
     let _unlock; // this is a function returned to caller to let them unlock once they're done
 
-    // represents our lock, to which only we hold the key to unlock
-    const lock = new Promise((resolve) => {
-      // our "unlock" function will unlock our lock (by resolving it)
+    //* represents a new "locked" lock, which will only be "unlocked" once its resolved
+    const newLock = new Promise((resolve) => {
+      // unlock will release this newLock (by resolving it)
       _unlock = () => resolve(); // explicitly call resolve() w/ no args to discard a malicious caller's args
     });
 
-    const { lock: whoToWaitFor } = _noLineUp() ? _mutex : _lastInLine(); // the lock we must wait for
-
-    // Once the task in front of us is done...
-    const key = whoToWaitFor.then(() => {
-      // THEN
-      _admitNext(); // we should be the next to go
-      return _unlock; // our key-promise resolves to our "unlock" function for our lock
-    });
-
-    _waitInLine(lock, key); // enter line up (even if it's empty)
-    return key; // return promise of unlock function, which caller should await on
+    // Caller is promised the lock (once it's unlocked)
+    // THEN, the promise resolves to the unlock function for the newLock
+    const promiseOfUnlock = _currentLock.then(() => _unlock);
+    // The next request will have to wait on this newLock (until caller is done and calls unlock)
+    _currentLock = newLock;
+    // Return promise of the unlock function, which caller should await on
+    return promiseOfUnlock;
   };
 
   /**
